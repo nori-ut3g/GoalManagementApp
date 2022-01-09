26422f2cd16f8681a229a49ae544fd3ecdfc84b8
nori-ut3g <akinori.nkd@gmail.com>
2022-01-09 12:38:41 +0900

    [add] ProgressBarComponent.vueを追加したが反映されず、後回し

--- public/js/app.js
+++ public/js/app.js
@@ -5298,6 +5298,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _TaskSectionComponent__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./TaskSectionComponent */ "./resources/js/components/TaskSectionComponent.vue");
+/* harmony import */ var _ProgressBarComponent__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./ProgressBarComponent */ "./resources/js/components/ProgressBarComponent.vue");
+//
 //
 //
 //
@@ -5312,10 +5314,46 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 
+
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   name: "ObjectiveBodyComponent",
   components: {
+    ProgressBarComponent: _ProgressBarComponent__WEBPACK_IMPORTED_MODULE_1__["default"],
     TaskSectionComponent: _TaskSectionComponent__WEBPACK_IMPORTED_MODULE_0__["default"]
+  }
+});
+
+/***/ }),
+
+/***/ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/ProgressBarComponent.vue?vue&type=script&lang=js&":
+/*!***************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/ProgressBarComponent.vue?vue&type=script&lang=js& ***!
+  \***************************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
+  name: "ProgressBarComponent",
+  data: function data() {
+    return {
+      progress: 44
+    };
   }
 });
 
@@ -28778,6 +28816,45 @@ component.options.__file = "resources/js/components/ObjectiveBodyComponent.vue"
 
 /***/ }),
 
+/***/ "./resources/js/components/ProgressBarComponent.vue":
+/*!**********************************************************!*\
+  !*** ./resources/js/components/ProgressBarComponent.vue ***!
+  \**********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _ProgressBarComponent_vue_vue_type_template_id_5aeefe62_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./ProgressBarComponent.vue?vue&type=template&id=5aeefe62&scoped=true& */ "./resources/js/components/ProgressBarComponent.vue?vue&type=template&id=5aeefe62&scoped=true&");
+/* harmony import */ var _ProgressBarComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./ProgressBarComponent.vue?vue&type=script&lang=js& */ "./resources/js/components/ProgressBarComponent.vue?vue&type=script&lang=js&");
+/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ "./node_modules/vue-loader/lib/runtime/componentNormalizer.js");
+
+
+
+
+
+/* normalize component */
+;
+var component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__["default"])(
+  _ProgressBarComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__["default"],
+  _ProgressBarComponent_vue_vue_type_template_id_5aeefe62_scoped_true___WEBPACK_IMPORTED_MODULE_0__.render,
+  _ProgressBarComponent_vue_vue_type_template_id_5aeefe62_scoped_true___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,
+  false,
+  null,
+  "5aeefe62",
+  null
+  
+)
+
+/* hot reload */
+if (false) { var api; }
+component.options.__file = "resources/js/components/ProgressBarComponent.vue"
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (component.exports);
+
+/***/ }),
+
 /***/ "./resources/js/components/TaskCardComponent.vue":
 /*!*******************************************************!*\
   !*** ./resources/js/components/TaskCardComponent.vue ***!
@@ -28888,6 +28965,22 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
+/***/ "./resources/js/components/ProgressBarComponent.vue?vue&type=script&lang=js&":
+/*!***********************************************************************************!*\
+  !*** ./resources/js/components/ProgressBarComponent.vue?vue&type=script&lang=js& ***!
+  \***********************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ProgressBarComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./ProgressBarComponent.vue?vue&type=script&lang=js& */ "./node_modules/babel-loader/lib/index.js??clonedRuleSet-5[0].rules[0].use[0]!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/ProgressBarComponent.vue?vue&type=script&lang=js&");
+ /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_babel_loader_lib_index_js_clonedRuleSet_5_0_rules_0_use_0_node_modules_vue_loader_lib_index_js_vue_loader_options_ProgressBarComponent_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__["default"]); 
+
+/***/ }),
+
 /***/ "./resources/js/components/TaskCardComponent.vue?vue&type=script&lang=js&":
 /*!********************************************************************************!*\
   !*** ./resources/js/components/TaskCardComponent.vue?vue&type=script&lang=js& ***!
@@ -28950,6 +29043,23 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "staticRenderFns": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_ObjectiveBodyComponent_vue_vue_type_template_id_600babed_scoped_true___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns)
 /* harmony export */ });
 /* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_ObjectiveBodyComponent_vue_vue_type_template_id_600babed_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./ObjectiveBodyComponent.vue?vue&type=template&id=600babed&scoped=true& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/ObjectiveBodyComponent.vue?vue&type=template&id=600babed&scoped=true&");
+
+
+/***/ }),
+
+/***/ "./resources/js/components/ProgressBarComponent.vue?vue&type=template&id=5aeefe62&scoped=true&":
+/*!*****************************************************************************************************!*\
+  !*** ./resources/js/components/ProgressBarComponent.vue?vue&type=template&id=5aeefe62&scoped=true& ***!
+  \*****************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "render": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_ProgressBarComponent_vue_vue_type_template_id_5aeefe62_scoped_true___WEBPACK_IMPORTED_MODULE_0__.render),
+/* harmony export */   "staticRenderFns": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_ProgressBarComponent_vue_vue_type_template_id_5aeefe62_scoped_true___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns)
+/* harmony export */ });
+/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_ProgressBarComponent_vue_vue_type_template_id_5aeefe62_scoped_true___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../../node_modules/vue-loader/lib/index.js??vue-loader-options!./ProgressBarComponent.vue?vue&type=template&id=5aeefe62&scoped=true& */ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/ProgressBarComponent.vue?vue&type=template&id=5aeefe62&scoped=true&");
 
 
 /***/ }),
@@ -29060,18 +29170,66 @@ var render = function () {
         [
           _c(
             "v-row",
-            _vm._l(3, function (n) {
-              return _c(
-                "v-col",
-                { key: n, attrs: { cols: "4" } },
-                [_c("TaskSectionComponent")],
-                1
-              )
-            }),
-            1
+            [
+              _c("progress-bar-component"),
+              _vm._v(" "),
+              _vm._l(3, function (n) {
+                return _c(
+                  "v-col",
+                  { key: n, attrs: { cols: "4" } },
+                  [_c("TaskSectionComponent")],
+                  1
+                )
+              }),
+            ],
+            2
           ),
         ],
         1
+      ),
+    ],
+    1
+  )
+}
+var staticRenderFns = []
+render._withStripped = true
+
+
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/ProgressBarComponent.vue?vue&type=template&id=5aeefe62&scoped=true&":
+/*!********************************************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./resources/js/components/ProgressBarComponent.vue?vue&type=template&id=5aeefe62&scoped=true& ***!
+  \********************************************************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "render": () => (/* binding */ render),
+/* harmony export */   "staticRenderFns": () => (/* binding */ staticRenderFns)
+/* harmony export */ });
+var render = function () {
+  var _vm = this
+  var _h = _vm.$createElement
+  var _c = _vm._self._c || _h
+  return _c(
+    "div",
+    [
+      _c(
+        "v-progress-linear",
+        {
+          attrs: { color: "blue-grey", height: "50" },
+          model: {
+            value: _vm.progress,
+            callback: function ($$v) {
+              _vm.progress = $$v
+            },
+            expression: "progress",
+          },
+        },
+        [_c("strong", [_vm._v(_vm._s(Math.ceil(_vm.progress)) + "%")])]
       ),
     ],
     1
--- resources/js/components/ObjectiveBodyComponent.vue
+++ resources/js/components/ObjectiveBodyComponent.vue
@@ -2,6 +2,7 @@
     <div>
         <v-container>
             <v-row>
+                <progress-bar-component></progress-bar-component>
                 <v-col cols=4 v-for="n in 3" :key="n">
                     <TaskSectionComponent></TaskSectionComponent>
                 </v-col>
@@ -13,9 +14,10 @@
 
 <script>
 import TaskSectionComponent from "./TaskSectionComponent";
+import ProgressBarComponent from "./ProgressBarComponent";
 export default {
     name: "ObjectiveBodyComponent",
-    components: {TaskSectionComponent}
+    components: {ProgressBarComponent, TaskSectionComponent}
 }
 </script>
 
