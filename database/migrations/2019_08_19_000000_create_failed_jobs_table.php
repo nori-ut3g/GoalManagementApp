3a958d32555a121a26db63284e027b21f5467137
nori-ut3g <akinori.nkd@gmail.com>
2022-01-25 00:03:57 +0900

    [fix] DO特有のエラー回避

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -15,7 +15,7 @@ class CreateFailedJobsTable extends Migration
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id()->primary();
-            $table->string('uuid')->unique();
+            $table->string('uuid');
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
