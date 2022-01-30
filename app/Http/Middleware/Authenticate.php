bb691cbdfd86319b046010d9c8f64dd39ddcb57b
nori-ut3g <akinori.nkd@gmail.com>
2022-01-30 21:11:20 +0900

    [change]未認証の場合AuthenticationExceptionを発生させるよう変更

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,13 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
-    /**
-     * Get the path the user should be redirected to when they are not authenticated.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
-     */
     protected function redirectTo($request)
     {
-        if (! $request->expectsJson()) {
-            return route('login');
-        }
+        throw new AuthenticationException;
     }
 }
