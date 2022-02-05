91a5661298553a3bff3f7d76ff9122e73c41e030
nori-ut3g <akinori.nkd@gmail.com>
2022-02-06 00:03:37 +0900

    [fix]UUID->IDの対応

--- app/Models/SharedObjective.php
+++ app/Models/SharedObjective.php
@@ -3,8 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-//use Illuminate\Database\Eloquent\Model;
-use GoldSpecDigital\LaravelEloquentUUID\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model;
 
 class SharedObjective extends Model
 {
